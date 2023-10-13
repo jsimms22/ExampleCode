dfae9db6efdd12a82aae9cabf0f1f0d458f8b496
jsimms22 <jpsimms2057@gmail.com>
2023-10-13 15:23:02 -0400

    added header comments for explaining program

--- omp/area.c
+++ omp/area.c
@@ -1,56 +1,65 @@
+/*
+  This program explores using parallel methods (openmp) to evaluate potential
+    performance and optimization increases over a serial implementation.
+
+  The methodolody employs a simply integration method to calculate pi.
+
+  Test Run Output:
+    pi = 3.14159
+    parallel time = 0.0120001 seconds
+    serial pi = 3.14159
+    serial time = 0.15 seconds
+*/
+
 #include <stdio.h>
 #include <omp.h>
 
-static long num_steps = 100000000;
-double step;
+static const unsigned long int num_steps = 100000000;
 
 int main() 
 {
-  //===========================================//
-  //------------------PARALLEL-----------------//
-  //===========================================//
+    //===========================================//
+    //------------------PARALLEL-----------------//
+    //===========================================//
   
-/* replaced by using omp_get_max_threads() in line 26
- * omp_get_max_threads gets as many threads available on the machine
- *
- * int numthreads = 12;
- */
-
-  register int i;
+/* 
+    replaced by using omp_get_max_threads() in line 26
+    omp_get_max_threads gets as many threads available on the machine
+    int numthreads = 12;
+*/
   double x, sum, pi = 0.0;
   double TIME1, TIME2;  
 
-  step = 1.0/(double) num_steps;
-  
+  const double step = 1.0/(double) num_steps;
+
   TIME1 = omp_get_wtime();
   omp_set_num_threads(omp_get_max_threads());
 
-/* #pragma omp parallel
- * {
- *    #pragme omp for
- * }  
- * ^^^^^^^^
- * -Is the same as the following active directive
- *
- * -Private makes variables private to each thread
- *
- * -Reduction makes local copy of the variable for each thread
- * then after the loop ends it adds the local copies into a
- * global variable of the same name.
- */
-#pragma omp parallel for private(x) reduction(+:sum)
-  for (i = 0; i < num_steps;i++)
-  {
-    x = (i+0.5)*step;
+/* 
+  #pragma omp parallel {
+      #pragme omp for
+  }  
+*/
+    // The above directive is the same as using the directive below
 
-/*didn't need this because of reduction(+:sum) was much faster*/
-//#pragma omp critical
-    sum += 4.0/(1.0+x*x);
+    // Private makes variables private to each thread
 
-/*didn't need this because of pragma omp for makes the iterater private*/
-//#pragma omp atomic
-//    i++;
-  }
+    // Reduction makes local copy of the variable for each thread
+    // then after the loop ends it adds the local copies into a
+    // global variable of the same name.
+
+  #pragma omp parallel for private(x) reduction(+:sum)
+    for (register unsigned long int i = 0; i < num_steps;i++) {
+      x = (i+0.5)*step;
+
+      // didn't need this because of reduction(+:sum) was much faster
+      //#pragma omp critical
+      sum += 4.0/(1.0+x*x);
+
+      // didn't need the below lines since 'pragma omp for' makes the iterater private
+      //#pragma omp atomic
+      //i++;
+    }
 
   TIME2 = omp_get_wtime();
   
@@ -61,15 +70,12 @@ int main()
   //===========================================//
   //------------------SERIAL-------------------//
   //===========================================//
-
-  i = 0;
   x = 0.0; 
   pi = 0.0;
   sum = 0.0;
   
   TIME1 = omp_get_wtime();
-  for (; i < num_steps;)
-  {
+  for (register unsigned long int i = 0; i < num_steps;) {
     x = (i+0.5)*step;
     sum = sum + 4.0/(1.0+x*x);
     i++;
--- omp/pi_monty.c
+++ omp/pi_monty.c
@@ -1,13 +1,34 @@
+/*
+  This program explores using parallel methods (openmp) to evaluate potential
+    performance and optimization increases over a serial implementation.
+
+  The methodolody employs a Monte-Carlo approach to calculate pi.
+
+  The Monty Carlo method for finding Pi works by calculating:
+   * random points inside a box, with width and height of 1.
+   * Points (n) to lie on or within a quarter circle of radius 1 is
+   * known to be n = (pi * N) / 4. Therefore we need to randomly
+   * generate an x and y coordinate and apply the rule of x*x + y*y <= 1 
+
+  Test Run Output:
+    thread count: 32 
+    parallel monte-carlo pi value: 3.14153 
+    runtime of parallel code: 1.617
+    serial monte-carlo pi value: 3.14158 
+    runtime of serial code: 1.675
+*/
+
 #include <stdio.h>
 #include <omp.h>
-#include <cstdlib>
+#include <stdlib.h>
 #include <time.h>
 
 static unsigned long int n = 100000000;
 
 int main (int argc, char *argv[])
 {
-  unsigned long int count, i = 0;
+  unsigned long int count = 0;
+  //unsigned long int i;
   long double x,y,pi = 0.0;
   double start,end;
   
@@ -38,7 +59,7 @@ int main (int argc, char *argv[])
    * runtime: determined at runtime
    * auto: we let the compiler determine wht the chunksize will be
    */
-#pragma omp parellel for private(x,y) reduction(+:count) schedule(static [,50])
+#pragma omp parellel for private(x,y) reduction(+:count) schedule(runtime/*static [,50]*/)
 {
   /* The Monty Carlo method for finding Pi works by calculating
    * random points inside a box, with width and height of 1.
@@ -47,8 +68,7 @@ int main (int argc, char *argv[])
    * generate an x and y coordinate and apply the rule of x*x + y*y <= 1 
    */
 
-  for (i = 0;i < n;i++)
-  {
+  for (unsigned long int i = 0;i < n;i++) {
     x = (double)rand()/RAND_MAX;
     y = (double)rand()/RAND_MAX;
     if (x*x + y*y <= 1.0) count++; 
@@ -73,8 +93,7 @@ int main (int argc, char *argv[])
   //===========================================//
 
   start = omp_get_wtime();
-  for (i = 0;i < n;i++)
-  {
+  for (unsigned long int i = 0;i < n;i++) {
     x = (double)rand()/RAND_MAX;
     y = (double)rand()/RAND_MAX;
     if (x*x + y*y <= 1.0) count++;
