fd2f16f8a2de453e09678417fa107071293a8b43
Jeremiah <jpsimms2057@gmail.com>
2023-12-30 12:40:18 -0600

    binary tree insert + print working

--- data structures/binary tree/binary_tree.cpp
+++ data structures/binary tree/binary_tree.cpp
@@ -8,6 +8,8 @@
 #include<iostream>
 #include<iomanip>
 
+#define COUNT 10
+
 struct Node // or class with public:
 {
     int m_value;
@@ -26,33 +28,105 @@ public:
     BinaryTree() :m_root{NULL} { }
     BinaryTree(int _value) 
         :m_root{new Node{_value,NULL,NULL,NULL}} { }
-    void insert(int _value);
+    void insert(Node* ptr, int _value);
     void search(); /* TODO */
+    Node* get_root() const { return m_root; }
 
 private:
     Node* m_root;
 };
 
-void BinaryTree::insert(int _value)
+void BinaryTree::insert(Node* root, int _value)
 // 
 {
     Node* temp = new Node{_value,NULL,NULL,NULL};
-
+    Node* ptr = root;
     // need to handle cases:
     // -root is null
     // -traverse and find appropriate child to place new node
     // -deteremine if a swap between root and temp is required
-    if (this->m_root == NULL) {
-        this->m_root = temp;
+    if (root == NULL) {
+        root = temp;
+        //delete(temp);
+        std::cout << "root = temp\n";
+        return;
+    } else if (_value <= root->m_value) {
+        if (root->m_left == NULL) {
+            root->m_left = temp;
+            //delete(temp);
+            std::cout << "root->m_left = temp\n";
+            return;
+        } else {
+            ptr = root->m_left;
+            //delete(temp);
+            std::cout << "ptr = left, calling insert()\n";
+            insert(ptr,_value);
+        }
+    } else if (_value > root->m_value) {
+        if (root->m_right == NULL) {
+            root->m_right = temp;
+            //delete(temp);
+            std::cout << "root->m_right = temp\n";
+            return;
+        } else {
+            ptr = root->m_right;
+            //delete(temp);
+            std::cout << "ptr = right, calling insert()\n";
+            insert(ptr,_value);
+        }
     } else {
-
+        delete(temp);
+        std::cout << "returning\n";
+        return;
     }
+}
+
+// Function to print binary tree in 2D
+// It does reverse inorder traversal
+void print2DUtil(Node* root, int space)
+{
+    // Base case
+    if (root == NULL) { return; }
+ 
+    // Increase distance between levels
+    space += COUNT;
+ 
+    // Process right child first
+    print2DUtil(root->m_right, space);
+ 
+    // Print current node after space
+    // count
+    std::cout << std::endl;
+    for (int i = COUNT; i < space; i++)
+        std::cout << " ";
+    std::cout << root->m_value << "\n";
+ 
+    // Process left child
+    print2DUtil(root->m_left, space);
+}
+ 
+// Wrapper over print2DUtil()
+void print2D(Node* root)
+{
+    // Pass initial space count as 0
+    print2DUtil(root, 0);
 }
 
 int main()
 {
     BinaryTree* btree = new BinaryTree(10);
+    btree->insert(btree->get_root(),5);
+    btree->insert(btree->get_root(),11);
+    btree->insert(btree->get_root(),11);
+    btree->insert(btree->get_root(),9);
+    btree->insert(btree->get_root(),4);
+    btree->insert(btree->get_root(),15);
+    btree->insert(btree->get_root(),12);
+    btree->insert(btree->get_root(),'a'); //inserts ASCII value for 'a' by converting char->int
 
-    
+    print2D(btree->get_root());
 
+    delete(btree);
+
+    return 0;
 }
