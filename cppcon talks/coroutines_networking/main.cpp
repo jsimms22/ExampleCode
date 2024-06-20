5d393425b74f52bbb34fceabe56a5713b1091ce4
Jeremiah Simmons <jpsimms2057@gmail.com>
2024-06-20 09:54:21 -0400

    adding example code from conf talks

---
+++ cppcon talks/coroutines_networking/main.cpp
@@ -0,0 +1,14 @@
+/* Jim Pascoe - ACCU 2022 - How to use c++20 coroutines for networking*/
+
+// Std Lib
+#include <iostream>
+#include <coroutine>
+#include <future>
+// Windows Socket + TCP headers
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+int main(int argc, char* argv[])
+{
+
+}
