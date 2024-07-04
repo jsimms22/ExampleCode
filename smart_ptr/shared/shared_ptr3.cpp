ae706422bfd512da91df7127a5b1a51ec754ddca
Jeremiah Simmons <jpsimms2057@gmail.com>
2024-07-03 22:18:13 -0400

    practicing smart pointers

---
+++ smart_ptr/shared/shared_ptr3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <functional>
+#include "shared_ptr3.h"
+
+struct D // deleter functor
+{
+    D() = default;
+    D(const D&) { std::cout << "D copy ctor.\n"; }
+    D(D&) { std::cout << "D non-const copy ctor.\n"; }
+    D(D&&) { std::cout << "D move ctor.\n"; }
+    void operator()(Foo* p) const
+    {
+        std::cout << "D is deleting a c-style heap-allocated Foo.\n";
+        destroyObj(p);
+    }
+};
+
+int main()
+{
+    D destructor;
+    // unique_ptr must know the type of the custom deleter
+    // std::unique_ptr<Foo,D> u_ptr(createObj(5,10.1),destructor);
+    // shared_ptr has a typed erased deleter, so simply pass to the constructor
+    std::shared_ptr<Foo> s_ptr(createObj(5,10.1),destructor);
+
+    return 0;
+}
